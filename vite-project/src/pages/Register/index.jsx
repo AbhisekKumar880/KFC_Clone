09adcb2646470207d7414f4e8d569006fdfc6d3a
sumit kumar mandal <sumitmandal330@gmail.com>
2023-08-30 00:50:59 +0530

    few update in register page

--- vite-project/src/pages/Register/index.jsx
+++ vite-project/src/pages/Register/index.jsx
@@ -23,13 +23,13 @@ const Register = () => {
       },
       validationSchema:yup.object({
              firstName:yup.string().max(15,'Must be 15 characters or less').required('First Name Required'),
-             lastName:yup.string().max(15,'Must be 15 characters or less').required('First Name Required'),
-             email:yup.string().email('Invalid email address').required('pls Dont forget me'),
-             password:yup.string().min(6,'Atleast 6 characters ').required('password is Required'),
-             address:yup.string().required('Address is Required'),
-             city:yup.string().required('city is Required'),
-             state:yup.string().required('status is Required'),
-             zip:yup.string().required('zip is Required'),
+             lastName:yup.string().max(15,'Must be 15 characters or less').required('Last Name Required'),
+             email:yup.string().email('Invalid email address').required('Dont forget me'),
+             password:yup.string().min(6,'Atleast 6 characters ').required('password Required'),
+             address:yup.string().required('Address Required'),
+             city:yup.string().required('city Required'),
+             state:yup.string().required('status Required'),
+             zip:yup.string().required('zip Required'),
       }),
       onSubmit:(values)=>{
         console.log(values);
@@ -66,21 +66,21 @@ const Register = () => {
               FIRST NAME
             </label>
             <input type="text" onChange={handleChange} value={values.firstName} onBlur={handleBlur} name="firstName" className="form-control" id="firstName" />
-            <p className="error">{errors.firstName}</p>
+            <p className="error">{touched.firstName && errors.firstName ? errors.firstName:null}</p>
           </div>
           <div className="col-md-6">
             <label htmlFor="lastName" className="form-label">
               LAST NAME
             </label>
             <input type="text" onChange={handleChange} value={values.lastName} onBlur={handleBlur} name='lastName' className="form-control" id="lastName" />
-            <p className="error">{errors.lastName}</p>
+            <p className="error">{touched.lastName && errors.lastName ? errors.lastName:null}</p>
           </div>
           <div className="col-md-6">
             <label htmlFor="email" className="form-label">
               Email
             </label>
             <input type="email"  onChange={handleChange} value={values.email} onBlur={handleBlur} name="email" className="form-control" id="email" />
-            <p className="error">{errors.email}</p>
+            <p className="error">{touched.email && errors.email ? errors.email:null}</p>
           </div>
           <div className="col-md-6">
             <label htmlFor="password" className="form-label">
@@ -91,7 +91,7 @@ const Register = () => {
               className="form-control"
               id="password"
             />
-            <p className="error">{errors.password}</p>
+            <p className="error">{touched.password && errors.password ? errors.password:null}</p>
           </div>
           <div className="col-12">
             <label htmlFor="address" className="form-label">
@@ -103,7 +103,7 @@ const Register = () => {
               id="address"
               placeholder="1234 Main St"
             />
-            <p className="error">{errors.address}</p>
+            <p className="error">{touched.address && errors.address ? errors.address:null}</p>
           </div>
          
           <div className="col-md-6">
@@ -111,7 +111,7 @@ const Register = () => {
               City
             </label>
             <input type="text"  onChange={handleChange} value={values.city} onBlur={handleBlur} name="city" className="form-control" id="city" />
-            <p className="error">{errors.city}</p>
+            <p className="error">{touched.city && errors.city ? errors.city:null}</p>
           </div>
           <div className="col-md-4">
             <label htmlFor="state" className="form-label">
@@ -124,14 +124,14 @@ const Register = () => {
               <option>jharkhand</option>
               <option>West Bengal</option>
             </select>
-            <p className="error">{errors.state}</p>
+            <p className="error">{touched.state && errors.state ? errors.state:null}</p>
           </div>
           <div className="col-md-2">
             <label htmlFor="zip" className="form-label">
               Zip
             </label>
             <input type="text" onChange={handleChange} value={values.zip} onBlur={handleBlur}  name='zip' className="form-control" id="zip" />
-            <p className="error">{errors.zip}</p>
+            <p className="error">{touched.zip && errors.zip ? errors.zip:null}</p>
           </div>
           
           <div className="col-12">
